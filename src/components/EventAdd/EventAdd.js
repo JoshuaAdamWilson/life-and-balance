a48936392d13da2a76364ad667fa1562829929c8
Joshua Adam Wilson <50850874+JoshuaAdamWilson@users.noreply.github.com>
2022-04-26 19:18:08 -0700

    Edit Event Post to height

--- src/components/EventAdd/EventAdd.js
+++ src/components/EventAdd/EventAdd.js
@@ -1,15 +1,15 @@
-import React, { useState, useContext } from 'react'
-import "./EventAdd.css"
-import { Context } from "../../context/Context"
-import axios from "axios"
+import React, { useState, useContext } from "react";
+import "./EventAdd.css";
+import { Context } from "../../context/Context";
+import axios from "axios";
 
 const EventAdd = () => {
-  const [ title, setTitle ] = useState("")
-  const [ price, setPrice ] = useState("")
-  const [ location, setLocation ] = useState("")
-  const [ datetime, setDatetime ] = useState("")
-  const [ file, setFile ] = useState(null)
-  const { user } = useContext(Context)
+  const [title, setTitle] = useState("");
+  const [price, setPrice] = useState("");
+  const [location, setLocation] = useState("");
+  const [datetime, setDatetime] = useState("");
+  const [file, setFile] = useState(null);
+  const { user } = useContext(Context);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -18,73 +18,83 @@ const EventAdd = () => {
       title,
       location,
       price,
-      dateTime: datetime
-    }
+      dateTime: datetime,
+    };
     if (file) {
-      const data = new FormData()
-      const filename = Date.now() + file.name
-      data.append("name", filename)
-      data.append("file", file)
-      newEvent.photo = filename
+      const data = new FormData();
+      const filename = Date.now() + file.name;
+      data.append("name", filename);
+      data.append("file", file);
+      newEvent.photo = filename;
       try {
-        await axios.post("https://life-and-balance.herokuapp.com/api/upload", data)
+        await axios.post(
+          "https://life-and-balance.herokuapp.com/api/upload",
+          data,
+        );
       } catch (error) {
-        console.log(error)
+        console.log(error);
       }
     }
     try {
-      const res = await axios.post('https://life-and-balance.herokuapp.com/api/events', newEvent)
-      window.location.replace("/events")
+      const res = await axios.post(
+        "https://life-and-balance.herokuapp.com/api/events",
+        newEvent,
+      );
+      window.location.replace("/events");
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-    
-  }
+  };
   return (
     <div>
-      <form className="post-upload" onSubmit={handleSubmit}>
-        <div className="input-form">
+      <form className="event-upload" onSubmit={handleSubmit}>
+        <div className="event-input-form">
           <h1>Add Event</h1>
           <label>Add an Image: </label>
           <div className="uploadContainer">
-            <input 
+            <input
               type="file"
               id="file-input"
-              onChange={e => setFile(e.target.files[0])}
+              onChange={(e) => setFile(e.target.files[0])}
             />
             {file && (
-              <img className="uploadImage" src={URL.createObjectURL(file)} alt="" />
+              <img
+                className="uploadImage"
+                src={URL.createObjectURL(file)}
+                alt=""
+              />
             )}
           </div>
-          <input 
+          <input
             type="text"
             placeholder="Title"
             className="write-input-title"
             autoFocus={true}
-            onChange={e => setTitle(e.target.value)}
+            onChange={(e) => setTitle(e.target.value)}
           />
           <input
-            type="datetime-local" 
-            onChange={e => setDatetime(e.target.value)}
+            type="datetime-local"
+            onChange={(e) => setDatetime(e.target.value)}
           ></input>
-          <input 
+          <input
             type="text"
             placeholder="Location"
             className="write-input-title"
-            onChange={e => setLocation(e.target.value)}
+            onChange={(e) => setLocation(e.target.value)}
           />
-          <input 
+          <input
             type="number"
             placeholder="Price"
             className="write-input-title"
-            onChange={e => setPrice(e.target.value)}
+            onChange={(e) => setPrice(e.target.value)}
           />
-          <button className="write-submit" type="submit">Publish Event</button>
+          <button className="write-submit" type="submit">
+            Publish Event
+          </button>
         </div>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default EventAdd
-
+export default EventAdd;
